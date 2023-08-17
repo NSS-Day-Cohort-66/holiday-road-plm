c849e9e076c5b3b8126cbecfd3b2184b14a4578b
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-08-17 13:13:49 -0500

    transient state, event listeners, and handlers

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -1,16 +1,29 @@
+import { setParkChoice } from "../TransientState.js"
+
 export const renderParks = async () => {
     const response = await fetch("https://developer.nps.gov/api/v1/parks?limit=20&api_key=raQAwREdVS4V3isCCYzljmmPmg30rf9X3ZvfVZam")
     const parks = await response.json()
 
-    let parksHTML = `<h2>National Parks</h1>
-                    <div><select id="parks__dropdown">
+    let parksHTML = `<h2>National Parks</h2>
+                    <select id="parks_dropdown">
                     <option value="0">Parks yay!</option>`
-    const parksArray = parks.map(
+    let parkIdCounter = 0
+    const parksArray = parks.data.map(
         (park) =>  {
+            parkIdCounter ++
             return `
-            <option value=${park.parkCode}>${park.fullName}</option>`
+            <option value=${parkIdCounter}>${park.fullName}</option>`
         }
     )
     parksHTML += parksArray.join("")
+    parksHTML += `</select>`
     return parksHTML
 }
+
+const handleParkChoice = (choice) => {
+    if (choice.target.id === "parks_dropdown") {
+        setParkChoice(parseInt(choice.target.value))
+    }
+  }
+  
+  document.addEventListener("change", handleParkChoice)
