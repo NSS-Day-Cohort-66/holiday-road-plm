d6ba3dea8392a235c492e58ed5320e1eab5c013e
Michelle Primiani <primianimichelle@gmail.com>
2023-08-17 12:32:46 -0400

    All dropdown menus listed

---
+++ scripts/parks/ParkProvider.js
@@ -0,0 +1,17 @@
+export const renderParks = async () => {
+    const response = await fetch("https://developer.nps.gov/api/v1/parks?limit=20&api_key=raQAwREdVS4V3isCCYzljmmPmg30rf9X3ZvfVZam")
+    const parks = await response.json()
+
+    let parksHTML = `<h2>National Parks</h2>
+                    <select id="parks__dropdown">
+                    <option value="0">Parks yay!</option>`
+    const parksArray = parks.data.map(
+        (park) =>  {
+            return `
+            <option value=${park.parkCode}>${park.fullName}</option>`
+        }
+    )
+    parksHTML += parksArray.join("")
+    parksHTML += `</select>`
+    return parksHTML
+}
