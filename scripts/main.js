d422886e00ada68354ae4abb1faa1f1fcf939f57
Paolo Medel <paolomedel17@gmail.com>
2023-08-17 11:41:57 -0400

    Added HTML structure in Plum

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+import { renderHTML } from "./Plum.js";
+
 const mainContainer = document.querySelector("#container");
+
+const renderAllHTML = async () => {
+  mainContainer.innerHTML = await renderHTML();
+};
+
+renderAllHTML();
