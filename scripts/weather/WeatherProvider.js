71bb2ebe64ac1683fa61df9298cc15c34f5602ad
Paolo Medel <paolomedel17@gmail.com>
2023-08-18 15:27:21 -0400

    still need to popultate the details when clicked

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,31 +1,28 @@
-
 const handleWeather = async (changeEvent) => {
-  if (changeEvent.target.id === "parks_dropdown" &&
-      changeEvent.target.value !== 0) {
-        const latitude = changeEvent.target[parseInt(changeEvent.target.value)].dataset.latitude;
-        const longitude = changeEvent.target[parseInt(changeEvent.target.value)].dataset.longitude;
-        await renderWeather(latitude, longitude)
-        };
-      }
+  if (
+    changeEvent.target.id === "parks_dropdown" &&
+    changeEvent.target.value !== 0
+  ) {
+    const latitude =
+      changeEvent.target[parseInt(changeEvent.target.value)].dataset.latitude;
+    const longitude =
+      changeEvent.target[parseInt(changeEvent.target.value)].dataset.longitude;
+    await renderWeather(latitude, longitude);
+  }
+};
 
 export const renderWeather = async (latitude, longitude) => {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=729652beba72cb8ebe2d345b36b55b68&units=imperial`);
-    const weather = await response.json();
-    debugger
-    const weatherHTML = `Local Forecast: \n
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=729652beba72cb8ebe2d345b36b55b68&units=imperial`
+  );
+  const weather = await response.json();
+  const weatherHTML = `Local Forecast: \n
                           Feels like: \n
                           Humidity: \n
                           Temperature: ${weather.list[0].main.temp}°F
-                        `
-    let weatherContainer = document.querySelector(".weather_html")
-    weatherContainer.innerHTML = weatherHTML
-    // let temp = weather.list.main.temp
+                        `;
+  let weatherContainer = document.querySelector(".weather_html");
+  weatherContainer.innerHTML = weatherHTML;
 };
 
-document.addEventListener("change", handleWeather)
-
-// const apiKey = "d8aa50947e1221a3030151e38237da3d";
-//     const url = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     return data;
+document.addEventListener("change", handleWeather);
