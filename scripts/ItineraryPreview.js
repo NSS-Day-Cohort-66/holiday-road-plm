95901eefa6f824c9178ae7888d3718087a86ca59
Michelle Primiani <primianimichelle@gmail.com>
2023-08-21 11:34:29 -0400

    updating itinerarypreview

--- scripts/ItineraryPreview.js
+++ scripts/ItineraryPreview.js
@@ -1,17 +1,24 @@
-import { transientState } from './TransientState.js'; 
+import { setParkChoice, transientState } from './TransientState.js'; 
 
 
 
 
 export const parkSelection = () => {
     const selectedPark = transientState.parkId
-debugger
+
+
     if (selectedPark) {
         const parkContainer = document.getElementById("park_selection")
         parkContainer.innerHTML = `
         <h2>${selectedPark}</h2>`
     }
     return selectedPark
+}
+
+
+const chosenPark = `<h2>${selectedPark}</h2>>`
+if (chosenPark) {
+    setParkChoice(chosenPark); 
 }
 
 parkSelection()
