76bfe702373d384fec5828c822da82b8af055662
Michelle Primiani <primianimichelle@gmail.com>
2023-08-23 12:38:28 -0400

    css weather styling

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -17,7 +17,7 @@ export const renderWeather = async (latitude, longitude) => {
   const weather = await response.json();
 
   const formatDate = (dateString) => {
-      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      const options = { weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric' };
       return new Date(dateString).toLocaleDateString('en-US', options);
   };
 
@@ -28,16 +28,27 @@ export const renderWeather = async (latitude, longitude) => {
   let weatherHTML = '';
   const weatherContainer = document.querySelector(".weather_html");
 
-  for (const dayIndex of [0, 8, 16, 24, 32]) {
+  for (const dayIndex of [0, 7, 15, 23, 31, 39]) {
+    const temperature = Math.round(weather.list[dayIndex].main.temp);
+   
+    let textColor = '';
+    if (temperature >= 80) {
+        textColor = 'red';
+    } else if (temperature >= 60) {
+        textColor = 'white';
+    } else {
+        textColor = 'blue';
+    }
       weatherHTML += `
-          <div class="weather_day_${dayIndex / 8}">
+          <div class="weather_day_">
               <img class="weather_icon" src="${getWeatherIconUrl(weather.list[dayIndex].weather[0].icon)}" alt="Weather Icon">
-              <div class="weather_date">Date: ${formatDate(weather.list[dayIndex].dt_txt)}<br><br></div>
-              Local Forecast: ${Math.round(weather.list[dayIndex].main.temp)}°F<br>
-              Feels like: ${Math.round(weather.list[dayIndex].main.feels_like)}°F<br>
+              <div class="weather_date">${formatDate(weather.list[dayIndex].dt_txt)}<br></div>
+              <div class="weather_forecast" style="color: ${textColor};">
+              ${temperature}°F<br></div>
+              <div class="weather_small">Feels like: ${Math.round(weather.list[dayIndex].main.feels_like)}°F<br>
               Chance of Rain: ${Math.round(weather.list[dayIndex].pop * 100)}%<br>
               Humidity: ${weather.list[dayIndex].main.humidity}%<br>
-              Description: ${weather.list[dayIndex].weather[0].description}<br><br>
+              Description: ${weather.list[dayIndex].weather[0].description}<br><br></div>
           </div>`;
   }
 
@@ -45,3 +56,7 @@ export const renderWeather = async (latitude, longitude) => {
 };
 
 document.addEventListener("change", handleWeather);
+
+// 80 and above is red
+// 59-79 white 
+// 0-60 blue 
